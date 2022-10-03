8b0b6a16caa4fd7b6a60a287c860fb517cf4987e
Scill <realscill@gmail.com>
2022-10-03 12:17:05 -0400

    Add participant module for all player info

---
+++ src/participant.ts
@@ -0,0 +1,49 @@
+import { ApexID } from '../assets/config.js';
+import { getGameData } from './stats.js';
+
+/**
+ * Information about a Tournament Participant.
+ */
+export interface Participant {
+    apexID: ApexID,
+    gameData: GameData,
+    points: number,
+}
+
+/**
+ * Data about all games the Participant has played during an active Tournament.
+ */
+export interface GameData {
+    games: number,
+    wins: number,
+    kills: number,
+    damage: number
+}
+
+export async function update(participants: Participant[], startTime: number) {
+    for (const participant of participants) {
+        const gameData = await getGameData(participant.apexID, startTime);
+        let points = 0;
+
+        // Calculate points for the available matches.
+        for (const key in gameData) {
+            switch (key) {
+                case "wins":
+                    points += gameData[key] * 3;
+                    break;
+                case "kills":
+                    points += gameData[key];
+                    break;
+                case "damage":
+                    points += Math.floor(gameData[key] / 225);
+                    break;
+            }
+        }
+
+        participant.gameData = gameData;
+        participant.points = points;
+    }
+
+    // Sort the Array in place.
+    participants.sort((a, b) => a.points - b.points);
+}
