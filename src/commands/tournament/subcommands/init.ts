b3ec1544547594fba3e7a4147761eab5559463d6
Scill <realscill@gmail.com>
2022-10-04 18:44:44 -0400

    Add init command

--- src/commands/tournament/subcommands/init.ts
+++ src/commands/tournament/subcommands/init.ts
@@ -1,3 +1,6 @@
+import { CommandInteraction } from 'discord.js'
+import { Tournament, getTournament } from '../../../tournaments.js'
+
 export const commandName = 'init'
 export const description = 'Initializes the tournament channel and category.'
 
@@ -7,6 +10,12 @@ export function register(subcommand) {
         .setDescription(description)
 }
 
-export function execute(interaction) {
-    
+export async function execute(interaction: CommandInteraction) {
+    const tournament: Tournament = getTournament(interaction.guildId);
+    tournament.init(interaction.channelId, interaction.channel.parentId);
+
+    return interaction.reply({
+        content: 'Channel has been set as Tournament channel.',
+        ephemeral: true
+    })
 }
