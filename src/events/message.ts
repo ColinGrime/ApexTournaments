6227d5c3fa77689b1737ecf6ce6c5692dc5b8195
Scill <realscill@gmail.com>
2022-10-04 18:43:49 -0400

    Delete all messages in tournament channel

---
+++ src/events/message.ts
@@ -0,0 +1,12 @@
+import { guildData } from '../tournaments.js'
+
+export const name = 'messageCreate';
+export const once = false;
+
+export function execute(message) {
+    for (const guildID in guildData) {
+        if (guildID === message.guildId && guildData[guildID]['tournamentChannelID'] === message.channelId) {
+            message.delete();
+        }
+    }
+}
