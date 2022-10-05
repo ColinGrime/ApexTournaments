aec45f9d6f05b8740318275bf91e0a281a8059cd
Scill <realscill@gmail.com>
2022-10-05 02:27:44 -0400

    Add opt-in subcommand

--- src/commands/tournament/subcommands/opt-in.ts
+++ src/commands/tournament/subcommands/opt-in.ts
@@ -1,5 +1,6 @@
 import { CommandInteraction, SlashCommandSubcommandBuilder } from 'discord.js';
-import { Tournament } from '../../../tournaments';
+import { Tournament } from '../../../tournaments.js';
+import { createEmbed } from '../../../utils/discord-utils.js';
 
 export const commandName = 'opt-in'
 export const description = 'Opt-in to the tournament if one is available.'
@@ -10,6 +11,15 @@ export function register(subcommand: SlashCommandSubcommandBuilder) {
         .setDescription(description)
 }
 
-export function execute(interaction: CommandInteraction, tournament: Tournament) {
-    // tournament.optIn
+export async function execute(interaction: CommandInteraction, tournament: Tournament) {
+    if (await tournament.optIn(interaction.user.id)) {
+        return interaction.reply({ 
+            embeds: [createEmbed(`**${interaction.user.username} has opted into the tournament!**`)] 
+        });
+    }
+
+    return interaction.reply({
+        content: 'There was an **error** opting you into the tournament.\nAre you sure one\'s active?',
+        ephemeral: true
+    })
 }
