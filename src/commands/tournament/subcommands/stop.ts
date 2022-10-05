11dd4190f175052594bed7ecd38499db60730e41
Scill <realscill@gmail.com>
2022-10-05 02:27:34 -0400

    Add stop subcommand

--- src/commands/tournament/subcommands/stop.ts
+++ src/commands/tournament/subcommands/stop.ts
@@ -1,5 +1,7 @@
 import { CommandInteraction, SlashCommandSubcommandBuilder } from 'discord.js';
-import { Tournament } from '../../../tournaments';
+import { Participant } from '../../../participant.js';
+import { Tournament } from '../../../tournaments.js';
+import { createEmbed } from '../../../utils/discord-utils.js';
 
 export const commandName = 'stop'
 export const description = 'Stops the tournament and displays the top kills, damage, and top overall!'
@@ -10,6 +12,23 @@ export function register(subcommand: SlashCommandSubcommandBuilder) {
         .setDescription(description)
 }
 
-export function execute(interaction: CommandInteraction, tournament: Tournament) {
-    
+export async function execute(interaction: CommandInteraction, tournament: Tournament) {
+    await tournament.stop();
+
+    const participants: Participant[] = tournament.participants;
+    const rankings: string[] = ['**Current Rankings:**']
+
+    for (let i=0; i<participants.length; i++) {
+        const participant = participants.at(i);
+        const gameData = participant.gameData;
+
+        rankings.push(`**${i+1}.** **<@${participant.discordID}>** (**${participant.points}** points)`);
+        rankings.push(`:black_small_square: **${gameData.wins}** wins, **${gameData.kills}** kills, **${gameData.damage}** damage`);
+        rankings.push('');
+    }
+
+    return interaction.reply({
+        embeds: [createEmbed(...rankings)],
+        ephemeral: true
+    });
 }
