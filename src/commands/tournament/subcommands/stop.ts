200b4816f05ca518e63792e249dc869214adb925
Scill <realscill@gmail.com>
2022-10-05 13:05:01 -0400

    Fix timeout of stop reply

--- src/commands/tournament/subcommands/stop.ts
+++ src/commands/tournament/subcommands/stop.ts
@@ -1,4 +1,4 @@
-import { CommandInteraction, SlashCommandSubcommandBuilder } from 'discord.js';
+import { CommandInteraction, SlashCommandSubcommandBuilder, TextChannel } from 'discord.js';
 import { Participant } from '../../../participant.js';
 import { Tournament } from '../../../tournaments.js';
 import { createEmbed } from '../../../utils/discord-utils.js';
@@ -13,22 +13,27 @@ export function register(subcommand: SlashCommandSubcommandBuilder) {
 }
 
 export async function execute(interaction: CommandInteraction, tournament: Tournament) {
-    await tournament.stop();
+    tournament.stop().then(() => {
+        const participants: Participant[] = tournament.participants;
+        const rankings: string[] = ['**Final Rankings:**']
+    
+        for (let i=0; i<participants.length; i++) {
+            const participant = participants.at(i);
+            const gameData = participant.gameData;
+    
+            rankings.push(`**${i+1}.** **<@${participant.discordID}>** (**${participant.points}** points)`);
+            rankings.push(`:black_small_square: **${gameData.wins}** wins, **${gameData.kills}** kills, **${gameData.damage}** damage`);
+            rankings.push('');
+        }
 
-    const participants: Participant[] = tournament.participants;
-    const rankings: string[] = ['**Current Rankings:**']
-
-    for (let i=0; i<participants.length; i++) {
-        const participant = participants.at(i);
-        const gameData = participant.gameData;
-
-        rankings.push(`**${i+1}.** **<@${participant.discordID}>** (**${participant.points}** points)`);
-        rankings.push(`:black_small_square: **${gameData.wins}** wins, **${gameData.kills}** kills, **${gameData.damage}** damage`);
-        rankings.push('');
-    }
+        const channel: TextChannel = interaction.channel as TextChannel;
+        channel.send({
+            embeds: [createEmbed(...rankings)],
+        });
+    })
 
     return interaction.reply({
-        embeds: [createEmbed(...rankings)],
+        content: '*Stopping the tournament...*',
         ephemeral: true
     });
 }
