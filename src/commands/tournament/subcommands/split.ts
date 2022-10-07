521cf76829a427fcaaa93bc611001fa6bbb494f6
Scill <realscill@gmail.com>
2022-10-07 16:10:39 -0400

    Add split subcommand

---
+++ src/commands/tournament/subcommands/split.ts
@@ -0,0 +1,15 @@
+import { CommandInteraction, SlashCommandSubcommandBuilder } from 'discord.js';
+import { Tournament } from '../../../tournaments.js';
+
+export const commandName = 'split'
+export const description = 'Splits the participants up into separate voice calls.'
+
+export function register(subcommand: SlashCommandSubcommandBuilder) {
+    return subcommand
+        .setName(commandName)
+        .setDescription(description)
+}
+
+export function execute(interaction: CommandInteraction, tournament: Tournament) {
+    return interaction.reply(tournament.split());
+}
