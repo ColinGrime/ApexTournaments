79f2bf6a27b8fc11e8dbbb2d98c3a0e1fa203e91
Scill <realscill@gmail.com>
2022-10-07 16:07:38 -0400

    Add various channel functions

--- src/utils/discord-utils.ts
+++ src/utils/discord-utils.ts
@@ -1,4 +1,4 @@
-import { Guild, ChannelType, GuildChannel, TextChannel, EmbedBuilder } from 'discord.js';
+import { Guild, ChannelType, GuildChannel, TextChannel, EmbedBuilder, VoiceChannel } from 'discord.js';
 
 /**
  * Quick way to create a basic channel.
@@ -45,10 +45,57 @@ export async function renameChannel(guild: Guild, channelID: string, channelName
     return channel;
 }
 
+/**
+ * Quick way to make embeds.
+ * @param description description of the embed
+ * @returns EmbedBuilder
+ */
 export function createEmbed(...description: string[]): EmbedBuilder {
     return new EmbedBuilder()
         .setColor(0xCB251B)
         .setThumbnail('https://content.uiowa.edu/sites/content.uiowa.edu/files/apex-legends-logo-ftr_175s2k8gp3yw7106ov4jxxiadi.png')
         .setTitle(':fire: :fire: :fire:     **__Apex Tournament__**     :fire: :fire: :fire:')
         .setDescription('​\n' + description.join('​\n'))
+}
+
+/**
+ * Send an embed to a text channel.
+ * @param channel text channel to send to
+ * @param args string arguments to send
+ */
+export function sendToChannel(channel: TextChannel, ...args: string[]) {
+    channel.send({
+        embeds: [createEmbed(...args)]
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+/**
+ * Send an embed to a text channel.
+ * @param channel text channel to send to
+ * @param embed embed to send to the text channel
+ */
+export function sendEmbedToChannel(channel: TextChannel, embed: EmbedBuilder) {
+    channel.send({
+        embeds: [embed]
+    }).catch(err => {
+        console.log(err);
+    })
+}
+
+/**
+ * Moves users to a channel.
+ * Won't do anything if the user is not in a voice channel.
+ * 
+ * @param channel voice channel to send the users to
+ * @param discordIDs discord IDs to send to the channel
+ */
+ export function moveUsersToChannel(channel: VoiceChannel, ...discordIDs: string[]) {
+    const cache = channel.guild.members.cache;
+    for (const discordID of discordIDs) {
+        cache.get(discordID).voice.setChannel(channel).catch(err => {
+            console.log(err);
+        })
+    }
 }
