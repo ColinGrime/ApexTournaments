6624b1bbe6cb1ce5ec10fdc92b40412c0a3b7ba6
Scill <realscill@gmail.com>
2022-10-07 10:55:51 -0400

    Add rankings message to participants module

--- src/commands/tournament/subcommands/stop.ts
+++ src/commands/tournament/subcommands/stop.ts
@@ -1,5 +1,5 @@
 import { CommandInteraction, SlashCommandSubcommandBuilder, TextChannel } from 'discord.js';
-import { Participant } from '../../../participant.js';
+import { getRankings } from '../../../participant.js';
 import { Tournament } from '../../../tournaments.js';
 import { createEmbed } from '../../../utils/discord-utils.js';
 import messages from '../../../../assets/messages.js';
@@ -15,22 +15,11 @@ export function register(subcommand: SlashCommandSubcommandBuilder) {
 
 export async function execute(interaction: CommandInteraction, tournament: Tournament) {
     tournament.stop().then(() => {
-        const participants: Participant[] = tournament.participants;
-        const rankings: string[] = ['**Final Rankings:**']
-    
-        for (let i=0; i<participants.length; i++) {
-            const participant = participants.at(i);
-            const gameData = participant.gameData;
-    
-            rankings.push(`**${i+1}.** **<@${participant.discordID}>** (**${participant.points}** points)`);
-            rankings.push(`:black_small_square: **${gameData.wins}** wins, **${gameData.kills}** kills, **${gameData.damage}** damage`);
-            rankings.push('');
-        }
-
-        const channel: TextChannel = interaction.channel as TextChannel;
-        channel.send({
-            embeds: [createEmbed(...rankings)],
+        tournament.channel.send({
+            embeds: [createEmbed("**Final Rankings:**", ...getRankings(tournament.participants))],
         });
+    }).catch(err => {
+        console.log(err);
     })
 
     return interaction.reply(messages.stopTournamentResponse);
--- src/participant.ts
+++ src/participant.ts
@@ -21,6 +21,11 @@ export interface GameData {
     damage: number
 }
 
+/**
+ * Updates the game data of each participant, calculates their points, and sorts them by point amount.
+ * @param participants list of participants
+ * @param startTime start time of a tournament
+ */
 export async function update(participants: Participant[], startTime: number) {
     for (const participant of participants) {
         const gameData = await getGameData(participant.apexID, startTime);
@@ -47,4 +52,24 @@ export async function update(participants: Participant[], startTime: number) {
 
     // Sort the Array in place.
     participants.sort((a, b) => b.points - a.points);
+}
+
+/**
+ * Gets a formatted message of the current rankings.
+ * @param participants list of participants
+ * @returns formatted message listing the current rankings in a tournament
+ */
+export function getRankings(participants: Participant[]): string[] {
+    const rankings: string[] = [];
+
+    for (let i=0; i<participants.length; i++) {
+        const participant = participants.at(i);
+        const gameData = participant.gameData;
+
+        rankings.push(`**${i+1}.** **<@${participant.discordID}>** (**${participant.points}** points)`);
+        rankings.push(`:black_small_square: **${gameData.wins}** wins, **${gameData.kills}** kills, **${gameData.damage}** damage`);
+        rankings.push('');
+    }
+
+    return rankings;
 }
