85023825e6f4952b8fb1ae3bebb66d72765cd4ba
Scill <realscill@gmail.com>
2022-10-07 16:07:11 -0400

    Rename stats module and add wheel functions

--- src/data-retriever.ts
+++ src/data-retriever.ts
@@ -1,9 +1,11 @@
 import axios from 'axios';
 import { GameData } from './participant';
+import { DiscordID } from '../assets/config';
 
-// Apex API requests that can be made.
+// Requests that can be made.
 const statisticsQueryByUID: string = `https://api.mozambiquehe.re/bridge?auth=${process.env.APEX_API}&uid=PLAYER_UID&platform=PC`;
 const matchHistoryQueryByUID: string =  `https://api.mozambiquehe.re/games?auth=${process.env.APEX_API}&uid=PLAYER_UID&mode=ARENAS&&start=START_TIME`;
+const newSpinningWheel: string = `https://colingrimes.dev/teams/?players=%PLAYERS%&realtime`
 
 async function getPlayerStats(apexID: string): Promise<object> {
     return await axios.get(statisticsQueryByUID.replace('PLAYER_UID', apexID));
@@ -70,4 +72,32 @@ export async function getGameData(apexID: string, startTime: number): Promise<Ga
     }
 
     return gameData;
+}
+
+/**
+ * Gets wheel link with the specified users.
+ * @param usernames list of Discord usernames
+ * @returns response from HTTP request of the wheel
+ */
+export async function getWheel(usernames: string[]) {
+    return axios.get(newSpinningWheel.replace('%PLAYERS%', usernames.join(',')));
+}
+
+/**
+ * Gets the wheel data from a wheel link.
+ * @param link link to the wheel you want data from
+ * @returns object containing the teams
+ */
+export async function getWheelData(link: string) {
+    const res = (await axios.get(`${link}&teams`)).data;
+
+    // If the wheel is not complete, return null.
+    if (!res['isComplete']) {
+        return null;
+    }
+
+    return {
+        "team1": res['teams']['team1'],
+        "team2": res['teams']['team2']
+    }
 }
